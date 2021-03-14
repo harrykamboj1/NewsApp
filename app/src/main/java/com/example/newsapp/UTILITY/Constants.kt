a66f3d62549eda59c5dad1ac4717baa35dda10e5
Harnoor Singh <49549662+harrykamboj1@users.noreply.github.com>
2021-03-14 12:04:12 +0530

    Update Constants.kt

--- app/src/main/java/com/example/newsapp/UTILITY/Constants.kt
+++ app/src/main/java/com/example/newsapp/UTILITY/Constants.kt
@@ -3,7 +3,7 @@ package com.androiddevs.mvvmnewsapp.UTILITY
 class Constants {
 
     companion object{
-        const val API_KEY = "733e83a81ba5448d9bc49771014a1a60"
+        const val API_KEY = ""
         const val BASE_URL = "http://newsapi.org"
     }
 }
